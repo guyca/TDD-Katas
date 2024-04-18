80b19b02ec039d111591c024c88a69f92bf15eae
Guy Carmeli <>
2024-04-18 11:00:38 +0300

    fix a bug

--- src/stringCalculator/index.test.ts
+++ src/stringCalculator/index.test.ts
@@ -1,11 +1,11 @@
-import { Calculator } from ".";
+import { Calculator } from '.';
 
 describe('StringCalculator', () => {
   let calculator: Calculator;
 
   beforeEach(() => {
     calculator = new Calculator();
-  })
+  });
 
   it('returns 0 when string is empty', () => {
     expect(calculator.add('')).toBe(0);
@@ -14,7 +14,7 @@ describe('StringCalculator', () => {
   it.each([
     ['1', 1],
     ['2', 2],
-    ['3', 3]
+    ['3', 3],
   ])('returns the input as number when string length is 1', (input, expected) => {
     expect(calculator.add(input)).toBe(expected);
   });
@@ -27,8 +27,6 @@ describe('StringCalculator', () => {
     ['11,12', 23],
     ['0,0', 0],
     ['100,200', 300],
-    ['-1,-1', -2],
-    ['-10', -10]
   ])('returns the sum of the input numbers', (input, expected) => {
     expect(calculator.add(input)).toBe(expected);
   });
@@ -39,5 +37,27 @@ describe('StringCalculator', () => {
 
   it('can handle new lines between numbers', () => {
     expect(calculator.add('1\n2,3')).toBe(6);
+  });
+
+  it('can handle different delimiters', () => {
+    expect(calculator.add('\n1;2//;4')).toBe(7);
+  });
+
+  it.each([
+    ['-1', 'Negatives not allowed: -1'],
+    ['2,-3,-4', 'Negatives not allowed: -3, -4'],
+  ])('throws an error when a negative number is passed', (input, expected) => {
+    expect(() => calculator.add(input)).toThrow(expected);
+  });
+
+  it.each([
+    ['2,1001', 2],
+    ['1001,1001', 0],
+  ])('ignores numbers greater than 1000', (input, expected) => {
+    expect(calculator.add(input)).toBe(expected);
+  });
+
+  it('can handle delimiters of any length', () => {
+    expect(calculator.add('//[***]\n1***2***3')).toBe(6);
   });
 });
--- src/stringCalculator/index.ts
+++ src/stringCalculator/index.ts
@@ -1,13 +1,26 @@
 export class Calculator {
   public add(numbers: string): number {
     const parsed = this.parse(numbers);
-    return parsed.reduce((acc, num) => acc + num);
+    this.validate(parsed);
+    const filtered = this.filterUnsupportedNumbers(parsed);
+    return filtered.reduce((acc, num) => acc + num);
   }
 
   private parse(numbers: string): number[] {
     if (numbers === '') return [0];
     return numbers
-      .split(/[\n,]/)
+      .match(/-?\d+/g)
       .map((num) => parseInt(num));
+  }
+
+  private validate(numbers: number[]): void {
+    const negatives = numbers.filter(num => num < 0);
+    if (negatives.length > 0) {
+      throw new Error(`Negatives not allowed: ${negatives.join(', ')}`);
+    }
+  }
+
+  private filterUnsupportedNumbers(numbers: number[]): number[] {
+    return numbers.map(num => num > 1000 ? 0 : num);
   }
 }
